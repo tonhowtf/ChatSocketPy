e94f80db8f7f00ca90602f03333e1a84f838fe8e
Tonho <antonioreverso.com@gmail.com>
2025-05-07 19:24:09 -0300

    Implement client connection handling and data broadcasting in server

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import socket
+import socket, threading, pickle
 HOST = "localhost"
 PORT = 5000
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,6 +9,20 @@ print(f"Servidor rodando na porta: {PORT}")
 
 clients = []
 
+def receive_data(conn, addr):
+  clients.append(conn)
+  print(f"Um novo cliente se conectou: {addr}")
+
+  while True:
+    data = pickle.loads(conn.recv(1024))
+    print(data)
+    
+    for client in clients:
+      if conn != client:
+        conn.sendall(pickle.dumps(data))
+
+
 while True:
   conn, addr = server.accept()
-  clients.append(conn)
+  threading.Thread(target=receive_data, args=(conn, addr), daemon=True).start()
+  
